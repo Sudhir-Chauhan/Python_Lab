be01369e7e59fdc74ba9d8b15a60d7b63bd2e61f
Sudhir Chauhan <sc08200625@gmail.com>
2025-02-25 09:16:42 +0530

    Basic Operators in Python..

---
+++ Operators.py
@@ -0,0 +1,42 @@
+#Arthmetic Operators
+
+a = 10
+b = 5
+print("Arthmetic Operators: ")
+print("Addition: ", a+b)
+print("Subtraction: ", a-b) 
+print("Multiplication: ", a*b)
+print("Division: ", a/b)
+print("Modulus: ", a%b)
+
+#Comparison Operators
+print("Comparison Operators: ")
+print("Equal: ", a==b)
+print("Not Equal: ", a!=b)
+print("Greater than: ", a>b)
+print("Less than: ", a<b)
+print("Greater than or equal to: ", a>=b)
+print("Less than or equal to: ", a<=b)
+
+#Logical Operators
+print("Logical Operators: ")
+x = True
+y = False
+print("AND: ", x and y)
+print("OR: ", x or y)
+print("NOT: ", not x)
+
+#Assignment Operators
+print("Assignment Operators: ")
+a = 10
+a += 5
+print("Addition Assignment: ", a)
+a -= 5
+print("Subtraction Assignment: ", a)
+a *= 5
+print("Multiplication Assignment: ", a)
+a /= 5
+print("Division Assignment: ", a)
+a %= 5
+print("Modulus Assignment: ", a)
+
